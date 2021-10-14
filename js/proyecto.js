1db364c9dc61d6e10fd78e33fb3cf77890ba7a6c
Carbonero13 <carbonero13@gmail.com>
2021-10-14 01:45:27 -0300

    Actualizacion 13102021

--- js/proyecto.js
+++ js/proyecto.js
@@ -1,4 +1,27 @@
-const socios=[{id:1, nombre: "Enzo", apellido: "Ferrari", numerosocio: "1001"},
+class Socios {
+    constructor(nombre, apellido) {
+        // this.numeroSocio = parseInt(numeroSocio);
+        this.nombre = nombre.toLowerCase();
+        this.apellido = apellido.toLowerCase();
+    }
+    confirmarSocio() {
+        document.getElementById('labelMostrarSocio').innerHTML = this.nombre;
+    }
+}
+
+const socios = [];
+
+function cargarSocio() {
+    let nombreForm = document.getElementById('nombre').value;
+    let apellidoForm = document.getElementById('apellido').value;
+    socios.push(new Socios(nombreForm, apellidoForm))
+    socios.forEach(element => console.log(element))
+}
+
+
+
+
+/* const socios=[{id:1, nombre: "Enzo", apellido: "Ferrari", numerosocio: "1001"},
 {id:2, nombre: "Ferruccio", apellido: "Lamborghini", numerosocio: "1002"},
 {id:3, nombre: "August", apellido: "Horch", numerosocio: "1003"},
 {id:4, nombre: "Henry", apellido: "Ford", numerosocio: "1004"}
@@ -7,4 +30,4 @@ const socios=[{id:1, nombre: "Enzo", apellido: "Ferrari", numerosocio: "1001"},
 for ( const socio of socios){
     console.log (socio.nombre + " "+ socio.apellido);
   
-}
+} */
