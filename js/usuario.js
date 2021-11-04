40e5ac62f3ef8c79e75c6e0edf4dec321a747a37
Carbonero13 <carbonero13@gmail.com>
2021-11-04 17:39:30 -0300

    Actualizacion 04112021

--- js/usuario.js
+++ js/usuario.js
@@ -1,8 +1,12 @@
+
+
+let fecha= [new Date('December 1, 2021'), new Date('December 2, 2021')];
 const myDatePicker = MCDatepicker.create({
   el: '#calendario',
-  dateFormat: 'dddd, dd mmmm yyyy',
+
+  dateFormat: 'dddd, dd mmmm yyyy', 
   bodyType: 'modal',
-  customWeekDays: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+  customWeekDays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
   customMonths: [
     'Enero',
     'Febrero',
@@ -17,8 +21,71 @@ const myDatePicker = MCDatepicker.create({
     'Noviembre',
     'Diciembre'
   ],
+
   customOkBTN: 'Seleccionar',
   customClearBTN: 'Borrar',
-  customCancelBTN: 'Cancelar'
-})
+  customCancelBTN: 'Cancelar',
+  disableDates: [new Date('December 1, 2021')], // ex: [new Date(2019,11, 25), new Date(2019, 11, 26)]
+  markDates:fecha,
+  theme: {
+    date: {
+        active: {
+            default: {
+                foreground: 'rgb(0, 0, 0)'
+            },
+            picked: {
+                foreground: '#ffffff',
+                background: '#38ada9'
+            },
+            today: {
+                foreground: 'rgb(0, 0, 0)',
+                background: 'rgba(0, 0, 0, 0.2)'
+            }
+        },
+        inactive: {
+            default: {
+                foreground: 'rgba(0, 0, 0, 0.2)'
+            },
+            picked: {
+                foreground: '#38ada9',
+                background: '#38ada9'
+            },
+            today: {
+                foreground: 'rgba(0, 0, 0, 0.2)',
+                background: 'rgba(0, 0, 0, 0.2)'
+            }
+        },
+        marcked: {
+            foreground: '#FF0000',
+          
+        }
+    }
+}
+
+});
+//let fecha= new Date('December 1, 2021');
+
+console.log(fecha)
+
+
+
+const listinsocios = [];
+for (let i = 0; i < socios.length; i++) {
+  listinsocios[i] = primeraMayuscula(socios[i].nombre) + " " + primeraMayuscula(socios[i].apellido) + " | " + socios[i].numero;
+};
+
+$(function () {
+  $("#tags").autocomplete({
+    source: listinsocios
+  });
+});
+//Como hago para guardar sin usar estas funciones
+$("#tags").change(function () {
+  let socioselecionado = document.getElementById('tags').value;
+  sessionStorage.setItem("socioreserva", socioselecionado)
+  
+});
+
+myDatePicker.onSelect((date, formatedDate) => {sessionStorage.setItem("fechareserva", date); });
+
 
